d81c8b687b078fc05a08c6bc4eb6ba638c6ee6b9
goaulda <sabat.adam@gmail.com>
2020-07-25 15:09:11 +0200

    Init git project

---
+++ src/main/java/com/api/archeo/ArcheoApplication.java
@@ -0,0 +1,13 @@
+package com.api.archeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcheoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcheoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/archeo/ArcheoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.archeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcheoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
